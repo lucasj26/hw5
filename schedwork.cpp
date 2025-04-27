5929f16e56e20ac67a7a115f5e7d2f0baa86707b
Lucas Jerry <ljerry@usc.edu>
2025-04-26 22:30:03 -0700

    final submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& counter,
+    int row,
+    int col
+);
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +48,54 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    for (size_t i = 0; i < avail.size(); ++i) {
+        vector<Worker_T> row;
+        for (size_t j = 0; j < dailyNeed; ++j) {
+            row.push_back(INVALID_ID);
+        }
+        sched.push_back(row); 
+    }
+
+    vector<int> shiftCounter(avail[0].size(), 0);
+    return helper(avail,dailyNeed,maxShifts,sched,shiftCounter,0,0);
 
 
 
 }
 
+
+
+bool helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& counter,
+    int row,
+    int col
+)
+{
+    if(row==avail.size()){
+        return true;
+    }
+    if(col==dailyNeed){
+        return helper(avail,dailyNeed,maxShifts,sched,counter,row+1,0);
+    }
+    for(int worker=0;worker<avail[0].size();worker++){
+        bool shiftsLeft = counter[worker] < maxShifts;
+        if(shiftsLeft && avail[row][worker]
+            && find(sched[row].begin(), sched[row].end(), worker) == sched[row].end()){
+            sched[row][col] = worker;
+            counter[worker]++;
+            if(helper(avail,dailyNeed,maxShifts,sched,counter,row,col+1)){
+                return true;
+            }
+            counter[worker]--;
+            sched[row][col] = INVALID_ID;
+        }   
+        
+
+    }
+    return false;
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,11 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result,
+    size_t index);
 
 
 // Definition of primary wordle function
@@ -23,6 +28,75 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    set<string> result;
+    size_t index = 0;
+    std::string curr = in;
+    set<char> originalFloating(floating.begin(), floating.end());
+    wordleHelper(curr,floating, dict, result, index);
+
+    return result;
+
+
 }
 
 // Define any helper functions here
+
+
+void wordleHelper(
+    std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result,
+    size_t index)
+    {
+        
+
+        if(index==in.length()){
+            if(floating.empty() && dict.find(in) != dict.end()){
+                result.insert(in);
+            }
+            return;
+        }
+
+        if(in[index] != '-'){
+            wordleHelper(in,floating,dict,result,index+1);
+        }
+        else{
+
+            size_t dashesLeft = 0;
+            for(size_t i = index; i < in.length(); i++){
+                if(in[i] == '-'){
+                    ++dashesLeft;
+                }
+            }
+            set<char> used;
+            for(size_t i=0;i<floating.length();i++){
+                char curr = floating[i];
+                if(used.find(curr)== used.end()){
+                    used.insert(curr);
+                    in[index] = curr;
+                    string newFloating = floating;
+                    newFloating.erase(i,1);
+                    wordleHelper(in,newFloating,dict,result,index+1);
+                    in[index] = '-';
+
+                }
+                
+            }
+            if(dashesLeft > floating.length()){
+                set<char> remainingFloating(floating.begin(),floating.end());
+                for(size_t i=0;i<26;i++){
+                    char letter = 'a' + i;
+                    if(used.find(letter)==used.end() && remainingFloating.find(letter)==remainingFloating.end()){
+                        in[index] = letter;
+                        wordleHelper(in,floating,dict,result,index+1);
+                        in[index] = '-';
+                    }
+                    
+                }
+            }
+            
+        }
+        
+        
+    }
